f8b585965b481241e59f930e3df055c10bdb866e
Thomas Begush <tbegush@gmail.com>
2021-05-23 20:47:59 -0700

    beats me

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,9 +45,40 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".list-group").on("click", "p", function() {
+  var text = $(this).text().trim();
+  var textInput = $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+  .addClass("form-control")
+  .val(text);
+});
 
+$(".list-group").on("blur", "textarea", function() {
+// get the textarea's current value/text
+var text = $(this)
+  .val()
+  .trim();
+// recreate p element
+var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
 
+// replace textarea with p element
+$(this).replaceWith(taskP);
 
+// get the parent ul's id attribute
+var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+// get the task's position in the list of other li elements
+var index = $(this)
+  .closest(".list-group-item")
+  .index();
+});
+tasks[status][index].text = text;
+saveTasks();
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
