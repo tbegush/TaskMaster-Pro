fe7f901b6bade86c19fe4a1bcd4434e608612fb9
Thomas Begush <tbegush@gmail.com>
2021-05-25 18:28:17 -0700

    adding refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -288,3 +288,11 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
+
+//1000 * 1ms = 1 second.  1 sec * 60 = 1 minute.  
